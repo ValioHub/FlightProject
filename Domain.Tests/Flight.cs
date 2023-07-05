27d1ad73816ddc1c86ebc427231af6d8adbd2c97
Valentin <valentin.chivliev02@gmail.com>
2023-07-05 15:11:48 +0300

    Translating a Scenario to Test

---
+++ Domain.Tests/Flight.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Domain.Tests
+{
+    public class Flight
+    {
+        public int RemainningNumberOfSeats { get; set; }
+        public Flight(int seatCapacity)
+        {
+            RemainningNumberOfSeats = seatCapacity;
+        }
+        public void Book(string v1,int v2)
+        {
+            RemainningNumberOfSeats -= v2;
+        }
+    }
+}
--- FlightProject/UnitTest1.cs
+++ FlightProject/UnitTest1.cs
@@ -1,5 +1,7 @@
 using System;
 using Xunit;
+using Domain.Tests;
+using FluentAssertions;
 
 namespace FlightProject
 {
@@ -8,7 +10,11 @@ namespace FlightProject
         [Fact]
         public void Test1()
         {
+            var flight = new Flight(seatCapacity: 3);
 
+            flight.Book("email@email.com",1);
+
+            flight.RemainningNumberOfSeats.Should().Be(2);
         }
     }
 }
