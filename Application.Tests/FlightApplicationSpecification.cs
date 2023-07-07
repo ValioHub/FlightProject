f3dc7d45896bd6898a098b5337c48e5b37d88f81
Valentin <valentin.chivliev02@gmail.com>
2023-07-07 11:03:40 +0300

    Refactoring Booking Service

--- Application.Tests/FlightApplicationSpecification.cs
+++ Application.Tests/FlightApplicationSpecification.cs
@@ -6,15 +6,16 @@ using Data;
 using Domain;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using Application.Tests;
 
 namespace Application.Tests
 {
     public class FlightApplicationSpecification
     {
         [Theory]
-        [InlineData("email@email.com",2)]
+        [InlineData("email@email.com", 2)]
         [InlineData("emailTwo@email.com", 2)]
-        public void Books_flights(string passengerEmail, int numberOfSeats )
+        public void Books_flights(string passengerEmail, int numberOfSeats)
         {
             var entities = new Entities(new DbContextOptionsBuilder<Entities>()
                 .UseInMemoryDatabase("Flights").Options);
@@ -28,49 +29,8 @@ namespace Application.Tests
             bookingService.Book(new BookDto(
                 flightId: flight.Id, passengerEmail, numberOfSeats));
 
-            bookingService.FindBooking(flight.Id).Should().ContainEquivalentOf( 
+            bookingService.FindBooking(flight.Id).Should().ContainEquivalentOf(
                 new BookingRm(passengerEmail, numberOfSeats));
-        }
-    }
-    public class BookingService
-    {
-        public Entities Entities { get; set; }
-        public BookingService(Entities entities)
-        {
-            this.Entities = entities;
-        }
-        public void Book(BookDto bookDto)
-        {
-            var flight = Entities.Flights.Find(bookDto.FlightId);
-            flight.Book(bookDto.PassengerEmail, bookDto.NumberOfSeats);
-            Entities.SaveChanges();
-        }
-        public IEnumerable<BookingRm> FindBooking(Guid flightId)
-        {
-            return Entities.Flights.Find(flightId).BookingList.Select(booking =>
-            new BookingRm(booking.Email, booking.NumberOfSeats));
-        }
-    }
-    public class BookDto
-    {
-        public Guid FlightId { get; set; }
-        public string PassengerEmail { get; set; }
-        public int NumberOfSeats { get; set; }
-        public BookDto(Guid flightId, string passengerEmail, int numberOfSeats)
-        {
-            this.FlightId = flightId;
-            this.PassengerEmail = passengerEmail;
-            this.NumberOfSeats = numberOfSeats;
-        }
-    }
-    public class BookingRm
-    {
-        public string PassengerEmail { get; set; }
-        public int NumberOfSeats { get; set; }
-        public BookingRm(string passengerEmail, int numberOfSeats)
-        {
-            this.PassengerEmail = passengerEmail;
-            this.NumberOfSeats = numberOfSeats;
         }
     }
 }
---
+++ Application/BookingService.cs
@@ -0,0 +1,27 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Application
+{
+    public class BookingService
+    {
+        public Entities Entities { get; set; }
+        public BookingService(Entities entities)
+        {
+            Entities = entities;
+        }
+        public void Book(BookDto bookDto)
+        {
+            var flight = Entities.Flights.Find(bookDto.FlightId);
+            flight.Book(bookDto.PassengerEmail, bookDto.NumberOfSeats);
+            Entities.SaveChanges();
+        }
+        public IEnumerable<BookingRm> FindBooking(Guid flightId)
+        {
+            return Entities.Flights.Find(flightId).BookingList.Select(booking =>
+            new BookingRm(booking.Email, booking.NumberOfSeats));
+        }
+    }
+}
